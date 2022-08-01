ac2cdd338348514bc7da606f1cf5adbb634a2ab7
hafidluqman50 <hafidluqmanulhakim@gmail.com>
2022-08-02 00:12:52 +0800

    Update Import Siswa

--- app/Http/Controllers/Admin/SiswaController.php
+++ app/Http/Controllers/Admin/SiswaController.php
@@ -329,9 +329,9 @@ class SiswaController extends Controller
                             'wilayah'         => Str::slug($cells[10]->getValue(),'-'),
                             'status_delete'   => 0
                         ];
-
+                        // dd(Siswa::where('nisn',$cells[1]->getValue())->where('status_delete',0)->count());
                         if (Siswa::where('nisn',$cells[1]->getValue())->where('status_delete',0)->count() == 0) {
-                            Siswa::create($data_siswa);
+                            Siswa::updateOrCreate($data_siswa);
 
                             $id_siswa = Siswa::where('nisn',$cells[1]->getValue())->get()[0]->id_siswa;
                         }
@@ -395,8 +395,9 @@ class SiswaController extends Controller
                         $cells = $row->getCells();
                         if ($cells[1]->getValue() != '' && $cells[2]->getValue() != '') {
                             $check_siswa = Siswa::where('nisn',$cells[1]->getValue())->count();
+                            // dd($check_siswa);
                             if ($check_siswa == 0) {
-                                return redirect('admin/siswa/import/')->with('log','Siswa '.$cells[2].' tidak ditemukan di sheet Data Keluarga! mohon cek kembali data siswa');
+                                return redirect('admin/siswa/import/')->with('log','Siswa '.$cells[2]->getValue().' tidak ditemukan di sheet Data Keluarga! mohon cek kembali data siswa');
                             }
                             else {
                                 $id_siswa_ = Siswa::where('nisn',$cells[1]->getValue())->get()[0]->id_siswa;
@@ -405,13 +406,13 @@ class SiswaController extends Controller
                         else {
                             $id_siswa_ = session()->get('import_siswa')['id_siswa'];
                         }
-
-                        $check_siswa_keluarga = Siswa::where('slug_siswa',Str::slug($cells[3]->getValue(),'-'))->count();
+                        
+                        $check_siswa_keluarga = Siswa::where('slug_siswa',Str::slug($cells[2]->getValue(),'-'))->count();
                         if ($check_siswa_keluarga == 0) {
                             return redirect('/admin/siswa/import')->with('log','Siswa '.$cells[2]->getValue().' tidak ditemukan di sheet Data Keluarga! mohon cek kembali data siswa');
                         }
                         else {
-                            $id_siswa_keluarga = Siswa::where('slug_siswa',Str::slug($cells[3]->getValue(),'-'))->get()[0]->id_siswa;
+                            $id_siswa_keluarga = Siswa::where('slug_siswa',Str::slug($cells[2]->getValue(),'-'))->get()[0]->id_siswa;
                         }
 
                         $data_keluarga = [
