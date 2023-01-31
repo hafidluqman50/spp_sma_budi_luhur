aa1b1e7fc9985d3879ae0eb1a40ba4580bc2e452
hafidluqman50 <hafidluqmanulhakim@gmail.com>
2023-02-01 02:19:15 +0800

    Fix Error Command Telegram

--- app/Listeners/TelegramNotification.php
+++ app/Listeners/TelegramNotification.php
@@ -164,11 +164,11 @@ _Terima Kasih :)_
 
     public function commandHapus($chat_id)
     {
-        // TelegramData::where('chat_id',$chat_id)->delete();
+        TelegramData::where('chat_id',$chat_id)->delete();
         Telegram::sendMessage([
             'chat_id'    => $chat_id,
             'text'       => 'Nomor Orang Tua Untuk Notifikasi Telegram Telah Dihapus, Silahkan Ketik Command /start Dan Masukkan Nomor Orang Tua Kembali Jika Ingin Mendapatkan Notifikasi Tunggakan.
-_Terima Kasih :)_'
+_Terima Kasih :)_',
             'parse_mode' => 'Markdown'
         ]);
     }
@@ -189,7 +189,7 @@ _Terima Kasih :)_'
         if ($get_message == '/start') {
             $this->commandStart($chat_id);
         }
-
+        
         $this->inputPhoneNumber($chat_id,$get_message);
 
         if ($get_message == '/info_tunggakan') {
@@ -200,13 +200,13 @@ _Terima Kasih :)_'
             $this->commandNomorRekSekolah($chat_id);
         }
 
-        // if ($get_message == '/faq') {
-        //     $this->commandFaq($chat_id);
-        // }
+        if ($get_message == '/faq') {
+            $this->commandFaq($chat_id);
+        }
 
-        // if ($get_message == '/help') {
-        //     $this->commandHelp($chat_id);
-        // }
+        if ($get_message == '/help') {
+            $this->commandHelp($chat_id);
+        }
 
         if ($get_message == '/hapus') {
             $this->commandHapus($chat_id);
